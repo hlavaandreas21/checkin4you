f3ea2e5cd13af6a3c98193babc9875cd9eec21af
Skrolux <46719449+Skrolux@users.noreply.github.com>
2022-07-11 23:01:57 +0200

    Add logging to MailService

--- checkin4you/Server/Services/Implementations/MailService.cs
+++ checkin4you/Server/Services/Implementations/MailService.cs
@@ -9,25 +9,41 @@ namespace checkin4you.Server.Services.Implementations
 {
     public class MailService : IMailService
     {
+        private readonly ILogger<MailService> _logger;
+
         private readonly MailSettings _mailSettings;
-        public MailService(IOptions<MailSettings> mailSettings)
+
+        public MailService(
+            ILogger<MailService> logger,
+            IOptions<MailSettings> mailSettings)
         {
+            _logger = logger;
             _mailSettings = mailSettings.Value;
         }
+
         public async Task SendEmailAsync(MailRequest mailRequest)
         {
-            MimeMessage email = new();
-            email.Sender = MailboxAddress.Parse(_mailSettings.Mail);
-            email.To.Add(MailboxAddress.Parse(mailRequest.ToEmail));
-            email.Subject = mailRequest.Subject;
-            BodyBuilder builder = new();
-            builder.TextBody = mailRequest.Body;
-            email.Body = builder.ToMessageBody();
-            using SmtpClient smtp = new();
-            smtp.Connect(_mailSettings.Host, _mailSettings.Port, true);
-            smtp.Authenticate(_mailSettings.Mail, _mailSettings.Password);
-            await smtp.SendAsync(email);
-            smtp.Disconnect(true);
+            try
+            {
+                MimeMessage email = new();
+                email.Sender = MailboxAddress.Parse(_mailSettings.Mail);
+                email.To.Add(MailboxAddress.Parse(mailRequest.ToEmail));
+                email.Subject = mailRequest.Subject;
+                BodyBuilder builder = new();
+                builder.TextBody = mailRequest.Body;
+                email.Body = builder.ToMessageBody();
+                using SmtpClient smtp = new();
+                smtp.Connect(_mailSettings.Host, _mailSettings.Port, true);
+                smtp.Authenticate(_mailSettings.Mail, _mailSettings.Password);
+                await smtp.SendAsync(email);
+                smtp.Disconnect(true);
+
+                _logger.LogInformation("[MailService] Successfully sent confirmation E-Mail.", mailRequest);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("[MailService] Error while trying to send confirmation E-Mail", ex.Message);
+            }
         }
     }
 }
